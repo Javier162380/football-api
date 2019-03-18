38214b01bda0f5741ba374e1ff37a1b3ba9ee195
Javier162380 <javierllorente16@gmail.com>
2019-03-18 22:39:34 +0100

    solve typo in s3 definition

--- helpers/s3_configuration.go
+++ helpers/s3_configuration.go
@@ -4,6 +4,6 @@ const (
 	Bucket_name             = "go-scrapper-tfm"
 	Games_key               = "games_historical.json"
 	Players_information_key = "playersinformation.json"
-	Players_stats_key       = "playersstas.json"
+	Players_stats_key       = "playersstats.json"
 	Results_evolution_key   = "resultsevolution.json"
 )
